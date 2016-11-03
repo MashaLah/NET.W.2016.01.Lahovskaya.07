f850cf4700df0b8690f1a0fac2ac17dd7e18d77e
MashaLah <lahoska91@mail.ru>
2016-11-03 16:12:53 +0300

    Add Calculate.

--- Task1/Polynomial.cs
+++ Task1/Polynomial.cs
@@ -8,5 +8,61 @@ namespace Task1
 {
     public class Polynomial
     {
+        private readonly double[] coefficients;
+        private readonly double x;
+        //private int degree;
+        //public double X {private get; set; }
+        //public int Degree { get; set; }
+        public int Degree
+        {
+            get { return coefficients.Length; }
+        }
+        public double this[int i]
+        {
+            get
+            {
+                return coefficients[i];
+            }
+            set
+            {
+                coefficients[i] = value;
+            }
+        }
+        public Polynomial(double x, double[] coefficients)
+        {
+            this.x = x;
+            /*this.coefficients = new double[coefficients.Length];
+            coefficients.CopyTo(this.coefficients, 0);*/
+            this.coefficients = new double[coefficients.Length];
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                this[i] = coefficients[i];
+            }
+        }
+
+        public double Calculate()
+        {
+            double result = 0;
+            for(int i = 0; i < Degree; i++)
+            {
+                result = result + this[i] * Math.Pow(x, i);
+            }
+            return result;
+        }
+
+        public double[] CalculateSum(Polynomial firstPolynomial, Polynomial secondPolynomial)
+        {
+            Polynomial maxDegreePolynomial = firstPolynomial.Degree > secondPolynomial.Degree ? firstPolynomial : secondPolynomial;
+            Polynomial minDegreePolynomial = firstPolynomial.Degree < secondPolynomial.Degree ? firstPolynomial : secondPolynomial;
+            double[] resultArray = new double[maxDegreePolynomial.Degree];
+            for (int i = 0; i < minDegreePolynomial.Degree; i++)
+            {
+                resultArray[i] = firstPolynomial[i] + secondPolynomial[i];
+            }
+            
+            return resultArray;
+        }
     }
 }
+
+
---
+++ Task1Tests/PolynomialTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task1;
+
+namespace Task1Tests
+{
+    public class PolynomialTests
+    {
+        [Test]
+        public void Constructor_Test()
+        {
+            double x = 1;
+            double[] arr = { 1, 2, 3, 0 };
+            Polynomial p = new Polynomial(x, arr);
+            double[] pArr = new double[4];
+            for (int i = 0; i < 4; i++)
+            {
+                pArr[i] = p[i];
+            }
+            Console.WriteLine(pArr.ToString());
+        }
+
+        [Test]
+        public void Calculate_ValidData_ValidResult()
+        {
+            double x = 1;
+            double[] arr = { 1, 2, 3, 0 };
+            Polynomial p = new Polynomial(x, arr);
+            double actual = p.Calculate();
+            double expected = 6;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CalculateSum_ValidPolynomial_DoubleArray()
+        {
+
+        }
+    }
+}
